94f89f7ce77da836f4363a787916347007998169
Delphina Michael Emoin <delphina429@gmail.com>
2025-07-29 10:47:58 +0530

    d

--- src/components/Clients.jsx
+++ src/components/Clients.jsx
@@ -283,7 +283,7 @@ export default function Clients() {
         </motion.div>
 
         {/* Client Companies Grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-5 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
           {clientCompanies.map((company, idx) => (
             <motion.div
               key={company.id}
